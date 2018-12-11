d80977ca0cb63a70ea1de58cca6ace01c160f830
Filipe Natanael <filipenatanael@live.com>
2018-12-10 22:00:27 -0200

    test: added navbarheader stories

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,24 +1,32 @@
 import React from 'react';
-
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import reducer from '../reducers/index'
+import Main from '../components/Main';
+import NavbarHeader from '../components/NavbarHeader';
 
-import App from '../App'
+// To enable Redux DevTools Extension
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
-// import { Button, Welcome } from '@storybook/react/demo';
-//
-// storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
-//
-// storiesOf('Button', module)
-//   .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-//   .add('with some emoji', () => (
-//     <Button onClick={action('clicked')}>
-//     <span role="img" aria-label="so cool">
-//     😀 😎 👍 💯
-//     </span>
-//     </Button>
-// ));
+const store = createStore(reducer,
+  composeEnhancers(
+    applyMiddleware(thunk)
+  )
+);
 
-storiesOf('App', module)
-  .add('with drawer', () =>   <App></App>);
+const drawerWidth = 240;
+
+storiesOf('Main', module)
+  .addDecorator(story => <Provider store={store}>{story()}</Provider>)
+  .add('loading entire application', () => <Main />)
+
+storiesOf('NavbarHeader', module)
+  .addDecorator(story => <Provider store={store}>{story()}</Provider>)
+  .add('passing headerTitle', () =>
+    <NavbarHeader
+      headerTitle={'Reactnd Project Readable'}
+      drawerWidth={drawerWidth}></NavbarHeader>);
