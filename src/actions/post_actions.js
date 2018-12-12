cbb84e736ab2531472c5bee7841f284ba94f5a64
Filipe Natanael <filipenatanael@live.com>
2018-12-11 23:31:24 -0200

    feat: implemented fetch all posts

---
+++ src/actions/post_actions.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+axios.defaults.headers.common['Authorization'] = { 'Authorization': 'whatever-you-want', 'Accept': 'application/json', };
+
+export function fetchPosts() {
+  return dispatch => {
+    axios.get(`http://localhost:5001/posts`)
+    .then(res => dispatch(fetchPostsSuccess(res.data)));
+  }
+}
+
+function fetchPostsSuccess(data) {
+  return {
+    type: 'FETCH_POSTS',
+    payload: data
+  };
+}
+
+export function postSortOrder(sortType) {
+  return {
+    type: 'POST_SORT_ORDER',
+    payload: sortType
+  }
+}
