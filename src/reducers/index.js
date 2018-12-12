7d5be84c19ee8ebdcce30393848f8f8d2f870dfe
Filipe Natanael <filipenatanael@live.com>
2018-12-11 23:26:12 -0200

    feat: added new reducer for posts

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,12 @@
 import { combineReducers } from 'redux';
 import CategoriesReducer from './categories_reducer';
 import PostsReducer from './posts_reducer';
-
+import PostsSortReducer from './posts_sort_reducer';
 
 const rootReducer = combineReducers({
   categories: CategoriesReducer,
-  posts: PostsReducer
+  posts: PostsReducer,
+  postsOrder:PostsSortReducer
 });
 
 export default rootReducer;
