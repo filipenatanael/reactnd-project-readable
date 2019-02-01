1d76c18d02f4b27d31bad1b165673e2e1533eb37
Filipe Natanael <filipenatanael@live.com>
2019-02-01 11:50:09 -0200

    fix(TypeError): Cannot read property params

--- src/stories/index.js
+++ src/stories/index.js
@@ -4,13 +4,18 @@ import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
+import { MemoryRouter, withRouter } from 'react-router-dom';
 import thunk from 'redux-thunk';
 import reducer from '../reducers/index'
 import Main from '../components/Main';
 import NavbarHeader from '../components/NavbarHeader';
 
-// To enable Redux DevTools Extension
+/* To enable Redux DevTools Extension */
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+/* The match, history and location props are only passed to components rendered by a Route component.
+Other components that need access to these properties can be wrapped with a call to the withRouter HOC. */
+const TMain = withRouter(Main);
 
 const store = createStore(reducer,
   composeEnhancers(
@@ -21,8 +26,12 @@ const store = createStore(reducer,
 const drawerWidth = 240;
 
 storiesOf('Main', module)
-  .addDecorator(story => <Provider store={store}>{story()}</Provider>)
-  .add('loading entire application', () => <Main />)
+  .addDecorator(story => (
+    <Provider store={store}>
+      <MemoryRouter initialEntries={['/']}>{story()}</MemoryRouter>
+    </Provider>
+  ))
+  .add('Drawable behavior', () => <TMain />)
 
 storiesOf('NavbarHeader', module)
   .addDecorator(story => <Provider store={store}>{story()}</Provider>)
