023da2a2b9b33bdeab53408e8b9003afbe8a4e99
Filipe Natanael <filipenatanael@live.com>
2019-02-06 23:41:14 -0200

    refactor: restructuring <App> component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,76 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { withStyles } from '@material-ui/core/styles';
 import Main from './components/Main';
+import NewPost from './components/NewPost';
+import NavbarHeader from './components/NavbarHeader';
+import CategoriesContainner from './containers/Categories';
+
+const drawerWidth = 240;
 
 class App extends Component {
+  state = {
+    open: false,
+  };
+
+  onChangeDrawer = (status) => {
+    this.setState({
+      open: status
+    });
+  }
+
   render() {
+    const { classes, theme, match } = this.props;
+    const { open } = this.state;
+
     return (
       <BrowserRouter>
-      <div>
-        <Switch>
-          <Route path="/" exact component={Main} />
-          <Route path="/:category" exact component={props => <Main {...props} />} />
-        </Switch>
-      </div>
+        <div className={classes.root}>
+          <NavbarHeader onChangeDrawer={this.onChangeDrawer} />
+            <main
+              className={classNames(classes.content, {
+                [classes.contentShift]: open,
+              })}
+            >
+              <Switch>
+                <Route path="/" exact component={Main} />
+                <Route path="/post/new" exact component={NewPost} />
+                <Route path="/:category" exact component={props => <Main {...props} />} />
+              </Switch>
+            </main>
+        </div>
       </BrowserRouter>
     );
   }
 }
 
-export default App;
+App.propTypes = {
+  classes: PropTypes.object.isRequired,
+  theme: PropTypes.object.isRequired,
+};
+
+const styles = theme => ({
+  root: {
+    display: 'flex',
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing.unit * 3,
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: -drawerWidth,
+  },
+  contentShift: {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
+  }
+});
+
+export default withStyles(styles)(App);
