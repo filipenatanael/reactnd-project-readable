4fc482b7896b309e81b79bd9abec903318e34717
Filipe Natanael <filipenatanael@live.com>
2019-02-06 23:41:44 -0200

    refactor: restructuring <NavbarHeader> component

--- src/components/NavbarHeader.js
+++ src/components/NavbarHeader.js
@@ -1,43 +1,84 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { withStyles } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
+import Drawer from '@material-ui/core/Drawer';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import  CssBaseline from '@material-ui/core/CssBaseline';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 
+import CategoriesContainner from '../containers/Categories';
+
+const drawerWidth = 240;
 
 class NavbarHeader extends Component {
+  state = {
+    open: false,
+  };
+
+  handleDrawerOpen = () => {
+    this.setState({ open: true });
+    this.props.onChangeDrawer(true)
+  };
+
+  handleDrawerClose = () => {
+    this.setState({ open: false });
+    this.props.onChangeDrawer(false)
+  };
+
   render() {
-    const { classes } = this.props;
-    const { headerTitle, open, onHandleDrawerOpen } = this.props;
+    const { classes, theme } = this.props;
+    const { open } = this.state;
+
     return (
       <div>
         <CssBaseline />
-        <AppBar
+          <AppBar
           position="fixed"
           className={classNames(classes.appBar, {
             [classes.appBarShift]: open,
           })}
-        >
+          >
           <Toolbar disableGutters={!open}>
-            <IconButton
-              color="inherit"
-              aria-label="Open drawer"
-              onClick={onHandleDrawerOpen}
-              className={classNames(classes.menuButton, open && classes.hide)}
-            >
+              <IconButton
+                color="inherit"
+                aria-label="Open drawer"
+                onClick={this.handleDrawerOpen}
+                className={classNames(classes.menuButton, open && classes.hide)}
+              >
               <MenuIcon />
             </IconButton>
             <Typography variant="h6" color="inherit" noWrap>
-              {headerTitle}
+              React Project Readable
             </Typography>
           </Toolbar>
-        </AppBar>
-        </div>
+          </AppBar>
+          <Drawer
+            className={classes.drawer}
+            variant="persistent"
+            anchor="left"
+            open={open}
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+          >
+            <div className={classes.drawerHeader}>
+              <IconButton onClick={this.handleDrawerClose}>
+                {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+              </IconButton>
+            </div>
+          <Divider />
+            <CategoriesContainner />
+          <Divider />
+        </Drawer>
+      </div>
     );
   }
 }
@@ -45,12 +86,26 @@ class NavbarHeader extends Component {
 NavbarHeader.propTypes = {
   classes: PropTypes.object.isRequired,
   theme: PropTypes.object.isRequired,
-  open: PropTypes.bool.isRequired,
-  drawerWidth: PropTypes.number.isRequired,
-  onHandleDrawerOpen: PropTypes.func.isRequired,
 };
 
-const styles = (theme, drawerWidth) => ({
+const styles = theme => ({
+  root: {
+    display: 'flex',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: '0 8px',
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+  },
   appBar: {
     transition: theme.transitions.create(['margin', 'width'], {
       easing: theme.transitions.easing.sharp,
