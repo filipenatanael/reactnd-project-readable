f99ff1c6117d3c0aaf16497adda14ff7b113abb7
Filipe Natanael <filipenatanael@live.com>
2019-02-10 15:54:41 -0200

    feat: adjusting categories icons and fonts

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -2,12 +2,34 @@ import React, { Component } from 'react';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import Favorite from '@material-ui/icons/Favorite';
+import Home from '@material-ui/icons/Home';
+import Share from '@material-ui/icons/Share';
+import School from '@material-ui/icons/School';
 import { Link } from 'react-router-dom';
 import _ from 'lodash';
 
 class Categories extends Component {
   componentWillMount() {
     this.props.fetchCategories()
+  }
+
+  renderIcon = (key) => {
+    switch (key) {
+      case 'react':
+        return <Favorite />
+      case 'redux':
+        return <Share />
+      case 'udacity':
+        return <School />
+      default:
+        return <Home />
+    }
+  }
+
+  capitalize = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
   }
 
   render() {
@@ -17,16 +39,20 @@ class Categories extends Component {
       return (
         <List>
           <ListItem>
-            <Link to="/">
-              <ListItemText primary="All" />
+            <ListItemIcon>{this.renderIcon('allposts')}</ListItemIcon>
+            <Link to="/"
+                  style={linkStyling}>
+              <ListItemText primary="All Posts" />
             </Link>
           </ListItem>
         {_.map(categories, category => {
           return (
             <ListItem button key={category.path}>
+              <ListItemIcon>{this.renderIcon(category.name)}</ListItemIcon>
               <Link to={`/${category.path}`}
-                    onClick={() => fetchCategoryPosts(category.path)}>
-                <ListItemText primary={category.name} />
+                    onClick={() => fetchCategoryPosts(category.path)}
+                    style={linkStyling}>
+                <ListItemText primary={this.capitalize(category.name)} />
               </Link>
             </ListItem>
           )
@@ -38,6 +64,11 @@ class Categories extends Component {
       <div>The Component is Loading...</div>
     );
   }
+}
+
+const linkStyling = {
+  paddingLeft: 10,
+  textDecoration: 'none'
 }
 
 export default Categories;
