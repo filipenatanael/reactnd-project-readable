6b432e28c9ff0a01e094ce659b194e80fe6f2645
Filipe Natanael <filipenatanael@live.com>
2019-02-10 18:44:40 -0200

    feat: creating comments reducer and action

---
+++ src/reducers/comments.js
@@ -0,0 +1,24 @@
+import _ from 'lodash';
+import  {
+  FETCH_POST_COMMENTS_COUNT,
+} from '../actions/types';
+
+const INITIAL_STATE = {
+  commentCount: {}
+};
+
+export default function (state = INITIAL_STATE, action) {
+  switch (action.type) {
+    case FETCH_POST_COMMENTS_COUNT:
+    const { id, amount } = action.payload;
+    return {
+      ...state,
+      commentCount: {
+        ...state['commentCount'],
+        [id]: amount
+      }
+    };
+    default:
+    return state;
+  }
+}
